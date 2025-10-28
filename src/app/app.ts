392ae382310e744dfba371e40f950af2a0a2d302
Deyan Dyankov <deyandyankov23@yahoo.com>
2025-10-28 22:10:56 +0100

    Homepage, Map, Header, Footer init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,15 @@
 import { Component, signal } from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {Header} from './header/header';
+import {Footer} from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    Header,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
